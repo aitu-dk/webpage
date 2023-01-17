b476dfcd31fac6ff3278912cfb2394e184ac7747
jonas-mika <accounts@jonassenghaas.de>
2023-01-17 12:14:06 +0100

    chore(*): updated project settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
   theme: {
     extend: {
       // custom colours
       colors: {
-        "light-bg": "##f9fafb",
-        "dark-bg": "#000000",
-        accent: "#9A4FBA",
+        accent: "#9a4fba",
+        accentDark: "#1f1025",
+        accentLight: "#cda7dd",
+        background: "#000000",
+        primary: "#ffffff",
+        secondary: "#adb5bd",
+        tertiary: "#6c757d",
       },
       // custom fonts
       fontFamily: {
@@ -18,12 +21,12 @@ module.exports = {
       // custom gradient
       backgroundImage: {
         // https://uigradients.com
-        amethyst: "linear-gradient(to right, #9d50bb, #6e48aa)",
+        amethyst: "linear-gradient(to top, #9d50bb, #6e48aa)",
       },
       // custom box shadow
       boxShadow: {
-        "light-accent": "0 0 20px 5px #552585",
-        "dark-accent": "0 0 20px 5px #9A4FBA",
+        accent: "0 0 20px 5px #9A4FBA",
+        "accent-small": "0 0 10px 3px #9A4FBA",
       },
     },
   },
