1f3a263932992c4df9fc9a7b3030e37a057e8b5c
jonas-mika <accounts@jonassenghaas.de>
2023-01-17 14:04:49 +0100

    feat(blog): added reading time remark plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,16 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// https://docs.astro.build/en/guides/markdown-content/#modifying-frontmatter-programmatically
+import { remarkReadingTime } from './remark-reading-time.mjs';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    extendDefaultPlugins: true,
+  }
 });
