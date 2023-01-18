c7b5ba504d62e9bd95dea552af23313dd7b2328c
jonas-mika <accounts@jonassenghaas.de>
2023-01-19 00:28:29 +0100

    fix(*): removed astro vercel adapter
    only needed for ssr sites, not for static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://docs.astro.build/en/guides/markdown-content/#modifying-frontmatter-programmatically
@@ -11,8 +8,6 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
