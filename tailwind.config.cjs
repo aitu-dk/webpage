e0173a81404ee5c84feddfe3a3464afeb33e2816
jonas-mika <accounts@jonassenghaas.de>
2023-01-19 12:36:51 +0100

    feat(style): changes to custom gradients, fonts,

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -16,8 +18,7 @@ module.exports = {
       },
       // custom fonts
       fontFamily: {
-        logo: "Righteous",
-        sans: ["DM Sans", "sans-serif"],
+        sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
       },
       // custom gradient
       backgroundImage: {
