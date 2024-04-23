9f6ad05ed041b52d0ceaee1731cb10d139a2b36d
Jan Tiegges <52353665+jantiegges@users.noreply.github.com>
2024-04-23 21:12:39 +0200

    implement ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,32 @@
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://docs.astro.build/en/guides/markdown-content/#modifying-frontmatter-programmatically
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
-// https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://docs.astro.build/en/guides/integrations-guide/vercel/
-import vercel from '@astrojs/vercel/static';
+// import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
+
+// https://astro.build/config
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bliss.berlin",
+  site: 'https://bliss.berlin',
   integrations: [tailwind(), react()],
   renderers: ['@astrojs/renderer-react'],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
-      wrap: true
+      wrap: true,
     },
     remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
-  output: 'static',
-  adapter: vercel({
-    analytics: true
-  })
+  output: 'server',
+  adapter: vercel(),
 });
